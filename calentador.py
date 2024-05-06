c9800bdcea2fc4bf2f61d12c46cb3eb325fec2fa
Tomas <tomilrclicciardi@gmail.com>
2024-05-06 20:23:06 -0300

    Mejorando la potencia y unidades

--- calentador.py
+++ calentador.py
@@ -1,127 +1,98 @@
 import matplotlib.pyplot as plt
 import math
+import numpy as np
 
+# Clase Proyecto y sus métodos como en tu código original...
 class Proyecto:
-    capacidad_calorifica = 4186 # J/kg°C
+    capacidad_calorifica = 4186  # J/kg°C
 
-    def __init__(self, temperatura_inicial, temperatura_final,temperatura_exterior):
-        self.capacidad = 1000                            # Gramos(volumen de agua en el cilindro)
-        self.temperatura_inicial = temperatura_inicial   # °C
-        self.temperatura_final = temperatura_final       # °C
-        self.temperatura_exterior = temperatura_exterior # °C
-        self.voltaje = 220                               # Voltaje
-        self.tiempo = 240                                # Segundos
-        self.altura = 35                                 # cm
-        self.radio = 8                                   # cm
-        self.espesor = 0.001                             # Metros
-        self.conductividad_de_telgopor = 0.035                                 # Conductividad térmica del telgopor
-        self.superficie = (2 * math.pi * (self.radio**2) + 2 * math.pi * self.radio * self.altura)/10000 #Area de la superficie del cilindro
+    def __init__(self, temperatura_inicial, temperatura_final, temperatura_exterior, resistencia=None):
+        self.temperatura_inicial = temperatura_inicial  # °C
+        self.temperatura_final = temperatura_final  # °C
+        self.temperatura_exterior = temperatura_exterior  # °C
+        self.capacidad = 1000  # Gramos (volumen de agua en el cilindro)
+        self.voltaje = 220  # Voltaje
+        self.tiempo = 240  # Segundos
+        self.altura = 35  # cm
+        self.radio = 8  # cm
+        self.espesor = 0.001  # Metros
+        self.conductividad_de_telgopor = 0.035  # Conductividad térmica del telgopor
+        self.superficie = (
+            (2 * math.pi * (self.radio ** 2) + 2 * math.pi * self.radio * self.altura) / 10000
+        )  # Área de la superficie del cilindro
+        self.resistencia = resistencia
 
+    # Métodos para cálculos
     def calcular_calor(self):
-        masa = self.capacidad
+        masa = self.capacidad/1000
         variacion_temperatura = self.temperatura_final - self.temperatura_inicial
         calor = masa * self.capacidad_calorifica * variacion_temperatura
         return calor
     
     def calcular_potencia(self):
-        potencia = self.calcular_calor() / self.tiempo
-        return potencia
-    
-    def calcular_intensidad(self):
-        intensidad = self.calcular_potencia() / self.voltaje
-        return intensidad
-    
-    def calcular_resistencia(self):
-        resistencia = self.voltaje / self.calcular_intensidad()
-        return resistencia
+        if self.resistencia:
+            potencia = ((self.voltaje ** 2) / self.resistencia)
+        else:
+            potencia = self.calcular_calor() / self.tiempo
+            #print(potencia)
+        return potencia 
     
     def temperatura_por_segundo(self):
         potencia = self.calcular_potencia()
-        aumento_temperatura = potencia / (self.capacidad * self.capacidad_calorifica)
+        aumento_temperatura = potencia / ((self.capacidad/1000) * self.capacidad_calorifica)
+        print(aumento_temperatura)
+        print(potencia)
         return aumento_temperatura
-
+    
     def obtener_temperaturas_sin_perdida(self):
         segundos = []
-        temperaturas_sin_perdida = []
+        temperaturas = []
         for seg in range(self.tiempo + 1):
             temperatura = self.temperatura_inicial + self.temperatura_por_segundo() * seg
             segundos.append(seg)
-            temperaturas_sin_perdida.append(temperatura)
+            temperaturas.append(temperatura)
         
-        return segundos, temperaturas_sin_perdida
-
+        return segundos, temperaturas
 
     def obtener_temperaturas_con_perdida(self):
         segundos = []
-        temperaturas_con_perdida = []
+        temperaturas = []
         temperatura_actual = self.temperatura_inicial
-
+        
         for seg in range(self.tiempo + 1):
-            print(seg)
-            calor_perdido = self.conductividad_de_telgopor * self.superficie * (temperatura_actual - self.temperatura_exterior) / self.espesor
-            print(calor_perdido)
+            calor_perdido = (
+                self.conductividad_de_telgopor * self.superficie * (temperatura_actual - self.temperatura_exterior) / self.espesor
+            )
             variacion_temperatura = self.temperatura_por_segundo() - (calor_perdido / self.capacidad_calorifica)
-            segundos.append(seg)
             temperatura_actual += variacion_temperatura
-            temperaturas_con_perdida.append(temperatura_actual)
-        print(temperaturas_con_perdida)
-        return segundos, temperaturas_con_perdida
-
-
-    def graficar_temperatura_sin_perdida(self):
-        segundos, temperaturas_sin_perdida = self.obtener_temperaturas_sin_perdida()
-        plt.plot(segundos, temperaturas_sin_perdida)
-        plt.xlabel('Tiempo (s)')
-        plt.ylabel('Temperatura (°C)')
-        plt.title('Temperatura del Líquido sin pérdida de calor')
-        plt.show()
-
-    def graficar_temperatura_con_perdida(self):
-        segundos, temperaturas_con_perdida = self.obtener_temperaturas_con_perdida()
-        plt.plot(segundos, temperaturas_con_perdida)
-        plt.xlabel('Tiempo (s)')
-        plt.ylabel('Temperatura (°C)')
-        plt.title('Temperatura del Líquido con pérdida de calor')
-        plt.show()
+            segundos.append(seg)
+            temperaturas.append(temperatura_actual)
+        
+        return segundos, temperaturas
 
 def main():
-    cantidad_proyectos = int(input("Ingrese la cantidad de proyectos a realizar: "))
-    proyectos = []
-    for i in range(cantidad_proyectos):
-        print(f"Proyecto {i + 1}")
-        while True:
-            try:
-                temperatura_inicial = int(input("Ingrese la temperatura inicial (en °C): "))
-                if temperatura_inicial < 0:
-                    raise ValueError("La temperatura inicial no puede ser negativa.")
-                break
-            except ValueError as e:
-                print(e)
+    temperatura_inicial = 20
+    temperatura_final = 80
+    temperatura_exterior = 20
+    resistencias = np.random.uniform(40, 50, 5)
+    opcion = int(input("1 para temperatura sin pérdida o 2 para temperatura con pérdida de calor: "))
+    
+    for i, resistencia in enumerate(resistencias):
+        proyecto = Proyecto(temperatura_inicial, temperatura_final, temperatura_exterior,resistencia)
         
-        while True:
-            try:
-                temperatura_final = int(input("Ingrese la temperatura final (en °C): "))
-                if temperatura_final < 0:
-                    raise ValueError("La temperatura final no puede ser negativa.")
-                break
-            except ValueError as e:
-                print(e)
-        
-        while True:
-            try:
-                temperatura_exterior = int(input("Ingrese la temperatura exterior (en °C): "))
-                if temperatura_exterior < 0:
-                    raise ValueError("La temperatura exterior no puede ser negativa.")
-                break
-            except ValueError as e:
-                print(e)
-        
-        proyecto = Proyecto(temperatura_inicial, temperatura_final, temperatura_exterior)
-        proyectos.append(proyecto)
-    plt.figure()
-    for pr in proyectos:
-        pr.graficar_temperatura_sin_perdida()
-        pr.graficar_temperatura_con_perdida()
+        if opcion == 1:
+            segundos, temperaturas = proyecto.obtener_temperaturas_sin_perdida()
+            plt.plot(segundos, temperaturas)
+        elif opcion == 2:
+            segundos, temperaturas = proyecto.obtener_temperaturas_con_perdida()
+            plt.plot(segundos, temperaturas)
 
-if __name__ == '__main__':
+    # Configuración del gráfico
+    plt.xlabel("Tiempo (s)")
+    plt.ylabel("Temperatura (°C)")
+    plt.title("Gráfico de Temperatura")
+    plt.legend() 
+    plt.show() 
+
+if __name__ == "__main__":
     main()
