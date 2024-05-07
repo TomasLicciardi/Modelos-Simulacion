6bda364c8b2a2f04b4094c1bf2a92be63b9855a0
Tomas <tomilrclicciardi@gmail.com>
2024-05-07 12:51:22 -0300

    Agregando TP 5 Y 6

--- calentador.py
+++ calentador.py
@@ -1,3 +1,4 @@
+import random
 import matplotlib.pyplot as plt
 import math
 import numpy as np
@@ -6,12 +7,12 @@ import numpy as np
 class Proyecto:
     capacidad_calorifica = 4186  # J/kg°C
 
-    def __init__(self, temperatura_inicial, temperatura_final, temperatura_exterior, resistencia=None):
+    def __init__(self, temperatura_inicial, temperatura_final, temperatura_exterior, resistencia=None,voltaje=None):
         self.temperatura_inicial = temperatura_inicial  # °C
         self.temperatura_final = temperatura_final  # °C
         self.temperatura_exterior = temperatura_exterior  # °C
         self.capacidad = 1000  # Gramos (volumen de agua en el cilindro)
-        self.voltaje = 220  # Voltaje
+        self.voltaje = voltaje if voltaje is not None else 220  # Voltios
         self.tiempo = 240  # Segundos
         self.altura = 35  # cm
         self.radio = 8  # cm
@@ -21,6 +22,7 @@ class Proyecto:
             (2 * math.pi * (self.radio ** 2) + 2 * math.pi * self.radio * self.altura) / 10000
         )  # Área de la superficie del cilindro
         self.resistencia = resistencia
+        self.voltaje = voltaje
 
     # Métodos para cálculos
     def calcular_calor(self):
@@ -60,26 +62,35 @@ class Proyecto:
         temperatura_actual = self.temperatura_inicial
         
         for seg in range(self.tiempo + 1):
-            calor_perdido = (
-                self.conductividad_de_telgopor * self.superficie * (temperatura_actual - self.temperatura_exterior) / self.espesor
-            )
-            variacion_temperatura = self.temperatura_por_segundo() - (calor_perdido / self.capacidad_calorifica)
-            temperatura_actual += variacion_temperatura
-            segundos.append(seg)
-            temperaturas.append(temperatura_actual)
+            num_aleatorio = random.randint(1,300)
+            if num_aleatorio == 1:
+                descenso_temperatura = random.randint(-50, 0)
+                duracion= random.randint(1, 40)
+                variacion_temperatura = descenso_temperatura / duracion
+                for i in range(duracion):
+                    segundos.append(seg)
+                    temperaturas.append(temperatura_actual)
+                    temperatura_actual += variacion_temperatura
+            else:
+                calor_perdido = (self.conductividad_de_telgopor * self.superficie * (temperatura_actual - self.temperatura_exterior) / self.espesor)
+                variacion_temperatura = self.temperatura_por_segundo() - (calor_perdido / self.capacidad_calorifica)
+                temperatura_actual += variacion_temperatura
+                segundos.append(seg)
+                temperaturas.append(temperatura_actual)
         
         return segundos, temperaturas
 
 def main():
-    temperatura_inicial = 20
     temperatura_final = 80
-    temperatura_exterior = 20
     resistencias = np.random.uniform(40, 50, 5)
+    temperaturas_iniciales = np.random.normal(10, 5, 5)
+    temperaturas_exteriores = np.random.uniform(-20,50,5)
+    voltajes = np.random.normal(220, 40, 5)
+    
     opcion = int(input("1 para temperatura sin pérdida o 2 para temperatura con pérdida de calor: "))
     
-    for i, resistencia in enumerate(resistencias):
-        proyecto = Proyecto(temperatura_inicial, temperatura_final, temperatura_exterior,resistencia)
-        
+    for i in range(5):
+        proyecto = Proyecto(temperaturas_iniciales[i],temperatura_final , temperaturas_exteriores[i],resistencias[i],voltajes[i])
         if opcion == 1:
             segundos, temperaturas = proyecto.obtener_temperaturas_sin_perdida()
             plt.plot(segundos, temperaturas)
